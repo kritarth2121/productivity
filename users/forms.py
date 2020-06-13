c111d386f91b96b8268fae71b339d4d3fcc17c2b
KRITARTH <kritarthsharma50@gmail.com>
2020-06-13 22:41:23 +0530

    fifth commit

--- users/forms.py
+++ users/forms.py
@@ -22,3 +22,4 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image','goal']
+        widgets = { 'goal': forms.Textarea(attrs={'rows':10, 'cols':20}),}
